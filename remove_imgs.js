d0371f1073a1256d568e70b49ced8e94bb0701a0
zaizen56u-lgtm <zaizen56u@gmail.com>
2026-04-11 17:07:21 +0900

    Delete remove_imgs.js

--- remove_imgs.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-let c = fs.readFileSync('public/index.html', 'utf8');
-c = c.replace(/<div class="side-image-column">[\s\S]*?<\/div>/g, '');
-c = c.replace(/<div class="side-image-wrapper">[\s\S]*?<\/div>/g, '');
-fs.writeFileSync('public/index.html', c);
-console.log('Done');
