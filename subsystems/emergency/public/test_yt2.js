cd4980d0aff96215c7f201bb79f28928219ea166
zaizen56u-lgtm <zaizen56u@gmail.com>
2026-09-15 15:02:30 +0900

    Delete subsystems directory

--- subsystems/emergency/public/test_yt2.js
+++
@@ -1,7 +0,0 @@
-async function getLive() {
-    const r = await fetch('https://www.youtube.com/@ANNnewsCH/live');
-    const t = await r.text();
-    const m = t.match(/<link rel="canonical" href="https:\/\/www\.youtube\.com\/watch\?v=([^"]+)"/);
-    console.log(m ? m[1] : 'Not found');
-}
-getLive();
--- subsystems/emergency/public/test_yt4.js
+++
@@ -1,7 +0,0 @@
-async function test() {
-    const r = await fetch('https://www.youtube.com/@FNNnewsCH/live');
-    const t = await r.text();
-    const m = t.match(/<link rel="canonical" href="https:\/\/www\.youtube\.com\/watch\?v=([^"]+)"/);
-    console.log('@FNNnewsCH', m ? m[1] : 'Not found');
-}
-test();
